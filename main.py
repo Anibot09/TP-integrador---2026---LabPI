2adedee3132b9c384d59f88bf21ad4aa51b86ed0
aniibot <ana.botello@escuelacangallo.edu.ar>
2026-05-27 23:17:47 -0300

    Modificacion del main

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ from clases.deposito import Deposito
 from clases.Productos.pan import Pan
 from clases.Productos.producto_gral import Producto_gral
 from clases.Productos.liquido import Liquido
+from clases.Productos.carne import Carne
 from clases.promociones import Promociones
 from clases.carrito import Carrito
 from clases.Gondolas.gondola import Gondola
 from clases.inventario import Inventario
 
 p = Pan(123, "flauta", "nona", 200, 3, 1, 1, 1)
-galle =  Producto_gral(712, "Melba", "Terrabusi", 400, 15, 2)
+#Productos tipo(Prod. gral.): Galletitas
+galletita_1 =  Producto_gral(712, "Melba", "Terrabusi", 400, 15, 2)
+galletita_2 =  Producto_gral(703, "Melba", "Terrabusi", 400, 15, 2)
+galletita_3 =  Producto_gral(708, "Melba", "Terrabusi", 400, 15, 2)
+#Productos tipo(Líquidos): Gaseosas
+bebida_1 =  Liquido(504, "Sprite", "Coca-Cola Company", 250, 13, 1, 500)
+bebida_2 =  Liquido(525, "Agua con gas", "Villa del Sur", 250, 13, 1, 500)
+bebida_3 =  Liquido(504, "Jugo", "Cepita", 250, 13, 1, 500)
+#Productos tipo: Carnes
+carne_1 = Carne(156, "Bandeja de Matambre", "-", 5000, 7, 2, 15)
+carne_2 = Carne(156, "Bandeja de Bondiola", "-", 5000, 7, 2, 15)
+carne_3 = Carne(156, "Bandeja de Vacío", "-", 15000, 7, 1, 15)
 perfume1 = Producto_gral(608, "mandarina", "ooo", 7230, 19, 2)
 perfume2 = Producto_gral(608, "magnolia", "iii", 9000, 2, 2)
-bebida =  Liquido(504, "Sprite", "Coca-Cola Company", 250, 13, 1, 500)
+
 #este funca
 #print(p)
 """
